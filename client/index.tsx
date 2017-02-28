968538dac6d89a9f60e328d000ac5aba500c7150
Unknown <russleyshaw@gmail.com>
2017-02-28 01:33:02 -0600

    Basic tsx

--- client/index.tsx
+++ client/index.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Hello } from "./hello";
-
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("root")
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
 );
