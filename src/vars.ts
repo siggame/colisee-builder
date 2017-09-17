047d0b34bbf10a9bc5ac123b575ef264a30076b1
user404d <conduffql@gmail.com>
2017-09-16 23:09:45 -0500

    Add registry url environment variable

--- src/vars.ts
+++ src/vars.ts
@@ -1,3 +1,4 @@
-import { defaultTo, toInteger } from "lodash";
+import { defaultTo, toInteger, toString } from "lodash";
 
 export const PORT: number = defaultTo<number>(toInteger(process.env.PORT), 3000);
+export const REGISTRY_URL: string = defaultTo<string>(toString(process.env.REGISTRY_URL), "localhost:5000");
