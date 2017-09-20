0908c255d0b592fe7a3592c5925bef7a70dc9f49
user404d <conduffql@gmail.com>
2017-09-19 22:54:38 -0500

    Fix using toInteger instead of toNumber

--- src/vars.ts
+++ src/vars.ts
@@ -1,4 +1,6 @@
-import { defaultTo, toInteger, toString } from "lodash";
+import { defaultTo, toNumber, toString } from "lodash";
 
-export const PORT: number = defaultTo<number>(toInteger(process.env.PORT), 3000);
-export const REGISTRY_URL: string = defaultTo<string>(toString(process.env.REGISTRY_URL), "localhost:5000");
+export const BUILD_INTERVAL: number = defaultTo<number>(toNumber(process.env.BUILD_INTERVAL), 1500);
+export const BUILD_LIMIT: number = defaultTo<number>(toNumber(process.env.BUILD_LIMIT), 10);
+export const PORT: number = defaultTo<number>(toNumber(process.env.PORT), 3000);
+export const REGISTRY_URL: string = defaultTo<string>(process.env.REGISTRY_URL, "localhost:5000");
