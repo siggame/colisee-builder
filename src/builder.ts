53d165df984369543ef1b3991c6bfa87943675d2
user404d <conduffql@gmail.com>
2018-02-13 18:57:36 -0600

    Updated to use queue for each team's submissions

--- src/builder.ts
+++ src/builder.ts
@@ -1,5 +1,6 @@
 import { db } from "@siggame/colisee-lib";
 import * as Docker from "dockerode";
+import { Deque } from "tstl";
 import * as winston from "winston";
 
 import { BUILD_INTERVAL, BUILD_LIMIT, REGISTRY_URL } from "./vars";
@@ -17,9 +18,11 @@ interface IBuildSubmission {
 
 interface IBuilderOptions {
     buildInterval: number;
-    queueLimit: number;
+    buildLimit: number;
     registry: string;
 }
+
+type BuildQueue = Deque<IBuildSubmission>;
 
 /**
  * Queues build submissions and builds them.
@@ -27,7 +30,7 @@ interface IBuilderOptions {
  * @class Builder
  */
 class Builder {
-    public submissions: Map<string, IBuildSubmission>;
+    public submissions: Map<string, BuildQueue>;
     private docker: Docker;
     private opts: IBuilderOptions;
     private runTimer: NodeJS.Timer | undefined;
@@ -37,10 +40,10 @@ class Builder {
      * @param {IBuilderOptions} [opts={ buildInterval: BUILD_INTERVAL, queueLimit: BUILD_LIMIT, registry: REGISTRY_URL }] 
      * @memberof Builder
      */
-    constructor(opts: IBuilderOptions = { buildInterval: BUILD_INTERVAL, queueLimit: BUILD_LIMIT, registry: REGISTRY_URL }) {
+    constructor(opts: IBuilderOptions = { buildInterval: BUILD_INTERVAL, buildLimit: BUILD_LIMIT, registry: REGISTRY_URL }) {
         this.docker = new Docker();
         this.opts = opts;
-        this.submissions = new Map<string, IBuildSubmission>();
+        this.submissions = new Map<string, BuildQueue>();
     }
 
     /**
@@ -53,15 +56,20 @@ class Builder {
     public run(): void {
         if (this.runTimer == null) {
             this.runTimer = setInterval(() => {
+                this.submissions.forEach((queue, _) => {
+                    if (queue.size() > 0 && (queue.front().status === "finished" || queue.front().status === "failed")) {
+                        queue.pop_front();
+                    }
+                });
                 const building = Array.from(this.submissions.values())
-                    .reduce((count, submission) =>
-                        submission.status === "building" ? count + 1 : count,
-                    0);
-                if (building < this.opts.queueLimit) {
+                    .reduce((count, queue) =>
+                        (queue.size() > 0 && queue.front().status === "building") ? count + 1 : count,
+                        0);
+                if (building < this.opts.buildLimit) {
                     Array.from(this.submissions.entries())
-                        .filter(([, submission]) => submission.status === "queued")
-                        .sort(([, subA], [, subB]) => subA.startedTime.getTime() - subB.startedTime.getTime())
-                        .slice(0, this.opts.queueLimit - building)
+                        .filter(([, queue]) => queue.size() > 0 && queue.front().status === "queued")
+                        .sort(([, [subA]], [, [subB]]) => subA.startedTime.getTime() - subB.startedTime.getTime())
+                        .slice(0, this.opts.buildLimit - building)
                         .forEach(([id]) => {
                             this.build(id);
                         });
@@ -92,42 +100,59 @@ class Builder {
      * @memberof Builder
      */
     private async build(id: string): Promise<void> {
-        const submission = this.submissions.get(id);
-        if (submission) {
-            const buildOutput: NodeJS.ReadableStream = await this.docker.buildImage(submission.context, { t: `${submission.tag}` });
-            // pipe build ouptut to log file or remote storage
-            submission.status = "building";
-            await db.connection("submissions").update({ status: submission.status }).where({ id: submission.id });
-            buildOutput.pipe(process.stdout);
-            buildOutput.on("error", async (error: any) => {
+        const queue = this.submissions.get(id);
+
+        if (queue == null || queue.size() === 0) {
+            winston.error(`No submissions for team ${id}`);
+        } else {
+            const submission = queue.front();
+
+            const handleError = async (error: any) => {
                 submission.status = "failed";
                 await db.connection("submissions").update({ status: submission.status }).where({ id: submission.id });
                 winston.error(error);
-            });
-            buildOutput.on("end", async () => {
-                winston.info(`successfully built ${submission.tag}`);
-                const image = await this.docker.getImage(submission.tag);
-                const pushOutput = await image.push({ "X-Registry-Auth": JSON.stringify({ serveraddress: this.opts.registry }) });
-                pushOutput.on("data", async (data: Buffer) => {
-                    const fields = JSON.parse(data.toString("utf-8"));
-                    if (fields.error) {
-                        pushOutput.emit("error", fields.error);
-                        pushOutput.removeAllListeners();
-                    }
+            };
+
+            let buildOutput: NodeJS.ReadableStream | undefined;
+            try {
+                buildOutput = await this.docker.buildImage(submission.context, { t: `${submission.tag}` });
+            } catch (error) {
+                submission.status = "failed";
+                await db.connection("submissions").update({ status: submission.status }).where({ id: submission.id });
+                winston.error(error);
+            }
+            submission.status = "building";
+            await db.connection("submissions").update({ status: submission.status }).where({ id: submission.id });
+            if (buildOutput) {
+                // TODO: replace with winston writing to enabled transport
+                buildOutput.pipe(process.stdout);
+                buildOutput.on("error", handleError);
+                buildOutput.on("end", async () => {
+                    winston.info(`successfully built ${submission.tag}`);
+                    const image = await this.docker.getImage(submission.tag);
+                    const pushOutput = await image.push({ "X-Registry-Auth": JSON.stringify({ serveraddress: this.opts.registry }) });
+                    // TODO: replace with winston writing to enabled transport
+                    pushOutput.pipe(process.stdout);
+                    pushOutput.on("error", handleError);
+                    pushOutput.on("end", async () => {
+                        submission.status = "finished";
+                        await db.connection("submissions").update({
+                            image_name: submission.tag,
+                            status: submission.status,
+                        }).where({ id: submission.id });
+                        winston.info(`successfully pushed ${submission.tag}`);
+                    });
                 });
-                pushOutput.on("error", async (error) => {
-                    submission.status = "failed";
-                    await db.connection("submissions").update({ status: submission.status }).where({ id: submission.id });
-                    winston.error(error);
-                });
-                pushOutput.on("end", async () => {
-                    submission.status = "finished";
-                    await db.connection("submissions").update({ status: submission.status }).where({ id: submission.id });
-                    winston.info(`successfully pushed ${submission.tag}`);
-                });
-            });
+            }
+        }
+    }
+
+    public async enqueue(id: string, submission: IBuildSubmission): Promise<void> {
+        const queue = this.submissions.get(id);
+        if (queue == null) {
+            this.submissions.set(id, new Deque<IBuildSubmission>(1, submission));
         } else {
-            winston.error(`No submission named ${name} `);
+            queue.push_back(submission);
         }
     }
 }
